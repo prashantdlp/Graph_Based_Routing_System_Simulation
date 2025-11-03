5f75235252fb6cf952ef90c0b1344a05f728f7ff
Reyansh <22reyanshshinde@gmail.com>
2025-11-03 17:59:19 +0000

    Updated Algorithm.cpp and Algorithm.hpp

---
+++ Phase-1/Algorithms.cpp
@@ -0,0 +1,21 @@
+#include "Algorithms.hpp"
+
+static std::tuple<bool, double, std::vector<int>> Shortest_paths(
+        const Graph& graph,
+        int source,
+        int target,
+        const std::string& mode,
+        const constraints& patch
+    ){
+
+    }
+static std::vector<int> KNN(
+    const Graph& graph,
+    double lat, double lon,
+    const std::string& poi,
+    const Node& node,
+    int k,
+    const std::string& metric
+    ){
+        
+    }
--- Phase-1/Algorithms.hpp
+++ Phase-1/Algorithms.hpp
@@ -5,21 +5,28 @@
 #include <vector>
 
 // Simple version - just functions, no helper structs
+struct constraints{
+    std::vector<int> forbidden_nodes;
+    std::vector<std::string> forbidden_road_types;
+};
 class Algorithms {
 public:
     // Returns: {found?, cost, path}
-    static std::tuple<bool, double, std::vector<int>> dijkstra(
+    static std::tuple<bool, double, std::vector<int>> Shortest_paths(
         const Graph& graph,
         int source,
         int target,
-        const std::string& mode
+        const std::string& mode,
+        const constraints& constraints
     );
     
-    static std::vector<int> knnEuclidean(
+    static std::vector<int> KNN(
         const Graph& graph,
         double lat, double lon,
         const std::string& poi,
-        int k
+        const Node& node,
+        int k,
+        const std::string& metric
     );
 };
 
