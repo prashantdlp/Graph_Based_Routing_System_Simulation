0f70eafb5f16b437575ceecc7867b4e91235ad30
Reyansh <22reyanshshinde@gmail.com>
2025-11-03 20:21:46 +0000

    Shortest_paths and KNN_euclidian_distance code

--- Phase-1/Algorithms.cpp
+++ Phase-1/Algorithms.cpp
@@ -1,14 +1,91 @@
 #include "Algorithms.hpp"
-#define M_PI 3.14159265358979323846
-static std::tuple<bool, double, std::vector<int>> Shortest_paths(
+#include <limits>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <functional>
+#include <algorithm>
+
+std::tuple<bool, double, std::vector<int>> Algorithms::Shortest_paths(
         const Graph& graph,
         int source,
         int target,
         const std::string& mode,
-        const constraints& patch
+        const constraints& constraints
     ){
+    int n = graph.size();
 
+    std::vector<int> forbidden_nodes = constraints.forbidden_nodes;
+    std::vector<std::string> forbidden_road_types = constraints.forbidden_road_types;
+
+    std::unordered_set<int> forbidden(
+    constraints.forbidden_nodes.begin(),
+    constraints.forbidden_nodes.end()
+    );
+    std::unordered_set<int> forbidden_r(
+    constraints.forbidden_road_types.begin(),
+    constraints.forbidden_road_types.end()
+    );
+
+    if (source < 0 || source >= n || 
+        target < 0 || target >= n ){
+        return {false, std::numeric_limits<double>::infinity(), {}};
     }
+    std::vector<bool> visited(n, false);
+    std::vector<double> distance(n, std::numeric_limits<double>::infinity());
+    std::vector<int> parent(n, -1);
+
+    std::priority_queue<
+        std::pair<double, int>,
+        std::vector<std::pair<double, int>>,
+        std::greater<std::pair<double, int>>
+    > pq;
+
+    distance[source] = 0.0;
+    pq.push({0.0, source});
+    while (!pq.empty()) {
+        auto [dist, v] = pq.top();
+        pq.pop();
+        
+        // Skip if visited
+        if (visited[v]) continue;
+        
+        visited[v] = true;
+        
+        // reached target
+        if (v == target) break;
+        
+        for (auto edge : graph.getAdjacentEdges(v)) {
+            if(forbidden_r.find(edge->id) == forbidden_r.end()){
+                int length = edge->length;  
+                int w = edge->v;
+                if (!visited[w] && forbidden.find(w) == forbidden.end()) {
+                    double new_dist = distance[v] + length;
+                    
+                    if (new_dist < distance[w]) {
+                        distance[w] = new_dist;
+                        parent[w] = v;
+                        pq.push({new_dist, w});
+                    }
+                }
+            }
+        }
+    
+    }
+    if (distance[target] == std::numeric_limits<double>::infinity()) {
+        return {false, std::numeric_limits<double>::infinity(), {}};
+    }
+    std::vector<int> path;
+    int current = target;
+    while (current != -1) {
+        path.push_back(current);
+        current = parent[current];
+    }
+    std::reverse(path.begin(), path.end());
+    
+    return {true, distance[target], path};
+}
+
 double euclidian_distance(double lat1, double lon1, double lat2, double lon2){
     const double REFERENCE_LATITUDE = 19.07; 
     const double DEG_TO_RAD = M_PI / 180.0;
@@ -23,7 +100,7 @@ double euclidian_distance(double lat1, double lon1, double lat2, double lon2){
     return std::sqrt(deltaX_meters * deltaX_meters + deltaY_meters * deltaY_meters);
 }
 
-static std::vector<int> KNN(
+std::vector<int> Algorithms::KNN(
     const Graph& graph,
     const std::string& poi,
     const Node& origin,
